2392660933e1e6cda365eb7aa62f30dd870f3098
Subhas Takhellambam <subhas@athena.edu>
2020-09-11 20:50:30 +0530

    About us page integration

---
+++ themes/custom/athena/webpack.prod.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: 'prodbundled.js',
+        path: path.resolve(__dirname, 'css')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'main.css' })
+    ], 
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", {
+                    loader: 'postcss-loader',
+                    options: {
+                      plugins: function () {
+                        return [
+                          require('autoprefixer')
+                        ];
+                      }
+                    }
+                  }, "sass-loader"]
+            }
+        ]
+    }
+});
