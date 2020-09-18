52e41f113102c4a672f860c3622585a8a21eed65
Subhas Takhellambam <subhas@athena.edu>
2020-09-18 21:20:45 +0530

    About us Integration post UI review

--- themes/custom/athena/assets/scripts/MobileMenu.js
+++ themes/custom/athena/assets/scripts/MobileMenu.js
@@ -2,16 +2,23 @@ class MobileMenu {
     constructor() {
         this.MenuIcon = document.querySelector(".site-header__menu-icon");
         this.MenuContent = document.querySelector(".site-header__menu-content");
+        this.AboutMenu = document.querySelector(".primary-nav .about a");
+        this.AboutSubMenu = document.querySelector(".about .secondary-nav");
         this.events();
     }
 
     events() {
         this.MenuIcon.addEventListener("click", () => this.toggleTheMenu());
+        this.AboutMenu.addEventListener("click", () => this.toggleTheAbout());
     }
 
     toggleTheMenu() {
         this.MenuContent.classList.toggle("site-header__menu-content--is-visible");
         this.MenuIcon.classList.toggle("site-header__menu-icon--close-x");
+    }
+
+    toggleTheAbout() {
+        this.AboutSubMenu.classList.toggle("show");
     }
 }
 
--- themes/custom/athena/webpack.prod.js
+++ themes/custom/athena/webpack.prod.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
     mode: 'production',
     output: {
         filename: 'prodbundled.js',
-        path: path.resolve(__dirname, 'css')
+        path: path.resolve(__dirname, '')
     },
     plugins: [
         new MiniCssExtractPlugin({ filename: 'main.css' })
