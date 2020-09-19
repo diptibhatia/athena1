9e7b0f8a48349fa58d8582322147586fc8143189
Subhas Takhellambam <subhas@athena.edu>
2020-09-19 15:00:21 +0530

    webpack compilation issue

--- themes/custom/athena/webpack.prod.js
+++ themes/custom/athena/webpack.prod.js
@@ -16,7 +16,7 @@ module.exports = merge(common, {
         rules: [
             {
                 test: /\.scss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", {
+                use: [MiniCssExtractPlugin.loader, "css-loader?url=false", {
                     loader: 'postcss-loader',
                     options: {
                       plugins: function () {
