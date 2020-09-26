34e93641fdd365bd01fbc6f47e21c7ecefdcd821
Mohammad <SMohamma@its.jnj.com>
2020-09-26 20:19:26 +0530

    Menu link for api data pull added

--- modules/custom/athena_course/src/Controller/AthenaCourseController.php
+++ modules/custom/athena_course/src/Controller/AthenaCourseController.php
@@ -1,6 +1,7 @@
 <?php
 namespace Drupal\athena_course\Controller;
 use Drupal\node\Entity\Node;
+use Drupal\Component\Serialization\Json;
 
 class AthenaCourseController {
   public function course($nid) {
@@ -24,9 +25,7 @@ $banner_block =  [
   '#banner' => $node->get('field_course_banner_image')->entity->uri->value,
   '#base_path' => $base_path,
   '#node' => $node
-];    
-
-
+]; 
 
 $course_description_tabs =  [
   '#theme' => 'course_description_tabs',
@@ -56,4 +55,60 @@ return array(
 );
 
   }
+
+  public function import(){
+
+   $nodes =  \Drupal::entityTypeManager()->getStorage('node')
+  ->loadByProperties(['type' => 'course', 'status' => 1]);
+
+
+ $client = \Drupal::service('http_client_factory')->fromOptions([
+      'base_uri' => 'http://3.7.173.255/athenadev/api/',
+    ]);
+
+    $response = $client->get('courselist');
+
+    $courses = Json::decode($response->getBody());
+    $items = [];
+    $node_ids = array();
+     foreach($nodes as $node_key => $node) {
+       $node_ids[] =  $node->get('field_cid')->value;
+      }
+
+    foreach ($courses as $key => $course_data) {
+      $cid = $course_data['cid'];
+       
+        if(!in_array($cid, $node_ids)) {
+          $new_node = Node::create(['type' => 'course']);
+          $new_node->set('title',$course_data['course_name']);
+          $new_node->set('field_course_total_credits', $course_data['ects_credit']);
+          $new_node->set('field_course_awarding_body', $course_data['awarded_by']);
+          $new_node->set('field_course_overview', $course_data['course_introduction']);
+          $new_node->set('field_cid', $cid);
+          $new_node->save();      
+        } else {
+               foreach($nodes as $node_key => $oldnode) {
+                if($oldnode->get('field_cid')->value == $cid) {
+                         $oldnode->set('title',$course_data['course_name']);
+            $oldnode->set('field_course_total_credits', $course_data['ects_credit']);
+             $oldnode->set('field_course_awarding_body', $course_data['awarded_by']);
+            $oldnode->set('field_course_overview', $course_data['course_introduction']);
+
+            $oldnode->save();
+                   //     break;
+                        }
+                }
+                
+
+ 
+       }
+    }
+
+return array(
+  '#markup' => '<h2>Course data updated from API</h2>',
+);
+
+  }
+  
+
 }
