13cf65f48e62a09f6c2213c91623a5938800edf0
Subhas Takhellambam <subhas.t@jigsawacademy.com>
2020-10-07 20:22:06 +0530

    insights pages

--- themes/custom/athena/assets/main.js
+++ themes/custom/athena/assets/main.js
@@ -1,6 +1,8 @@
 import 'bootstrap';
 import './styles/style.scss';
 import MobileMenu from './scripts/MobileMenu';
+import CoursePage from './scripts/CoursePage';
+import ExploreMenu from './scripts/ExploreMenu';
 
 
 if(module.hot) {
@@ -9,3 +11,5 @@ if(module.hot) {
 
 
 let mobileMenu = new MobileMenu();
+let coursePage = new CoursePage();
+let exploreMenu = new ExploreMenu();
