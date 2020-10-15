ac6bb616ab91011ead5ca00c3caa7ee8b2b931f9
Subhas Takhellambam <subhas.t@jigsawacademy.com>
2020-10-15 14:47:41 +0530

    social share using script

--- themes/custom/athena/script.js
+++ themes/custom/athena/script.js
@@ -1,6 +1,6 @@
-$(document).scroll(function() {
-    var y = $(document).scrollTop(),
-        header = $(".share-icon ul");
+jQuery(document).scroll(function() {
+    var y = jQuery(document).scrollTop(),
+        header = jQuery(".share-icon ul");
     if(y <= 1600)  {
         header.css({position: "fixed"});
     } else {
