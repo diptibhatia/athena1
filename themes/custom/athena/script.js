6797e1437d5f0c3bb9193eb4102b9a5d705ca0e1
Nitin Kumar <nitin.kumar1@tothenew.com>
2020-11-27 21:55:16 +0530

    UI changes

--- themes/custom/athena/script.js
+++ themes/custom/athena/script.js
@@ -1,4 +1,4 @@
-jQuery(document).scroll(function() {
+/*jQuery(document).scroll(function() {
     var y = jQuery(document).scrollTop(),
         header = jQuery(".share-icon ul");
     if(y <= 1600)  {
@@ -7,3 +7,24 @@ jQuery(document).scroll(function() {
         header.css("position", "relative");
     }
 });
+*/
+
+function scrollEvent(){
+        jQuery(function(){
+            var stopFloating = jQuery('.stopScroll');
+            if (stopFloating) {
+            var stop = stopFloating.offset().top;
+            jQuery(document).scroll(function() {
+                var y = jQuery(window).scrollTop(),
+                header = jQuery(".share-icon div");
+                if(y <= stop-500)  {
+                    header.css({display: "block"});
+                header.css({position: "fixed"});
+                } else {
+                    header.css({display: "none"});
+                }
+            });
+        }
+    });
+}
+window.onload = scrollEvent();
