78e24a531d740b5039d3d020e91ed0b7a500137f
Mohammad <SMohamma@its.jnj.com>
2020-11-30 20:12:45 +0530

    ui change

--- modules/custom/athena_course/src/Controller/AthenaCourseController.php
+++ modules/custom/athena_course/src/Controller/AthenaCourseController.php
@@ -364,8 +364,13 @@ foreach($paragraph_faq as $faq_data){
 
 }
 $banner = '';
+$banner_pic_url = '';
  if(is_object( $node->get('field_course_banner_image')->entity)){
 	$banner =$node->get('field_course_banner_image')->entity->uri->value;
+    $banner_pic = $node->get('field_course_banner_image')->entity->getFileUri();
+	$banner_pic_url = file_create_url($banner_pic);
+    }
+    
     }
 
 $banner_block =  [
@@ -376,7 +381,7 @@ $banner_block =  [
   '#awarding_body' => $node->get('field_course_awarding_body')->value,
   '#description' => $node->get('field_course_banner_description')->value,
   '#category' => $node->get('field_course_category')->value,
-  '#banner' => $banner,
+  '#banner' => $banner_pic_url,
   '#base_path' => $base_path,
   '#node' => $node
 ];
