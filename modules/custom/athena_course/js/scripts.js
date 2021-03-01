75ed3770af4b1ddf623573d6c51d55fe94ee2d77
Dipti Bhatia <dipti@athena.edu>
2021-03-01 15:51:25 +0530

    registration login AE-258

--- modules/custom/athena_course/js/scripts.js
+++ modules/custom/athena_course/js/scripts.js
@@ -327,6 +327,7 @@ jQuery.ajax(baseUrl + '/save/contact', {
                 var email_id = jQuery("#reg_email").val();
                 var cid = jQuery("#reg_course").val();
 
+
                 jQuery.ajax('https://athenawpapi.azurewebsites.net/Register/SaveLead', {
                   type: 'POST',  // http method
                   contentType: "application/json; charset=utf-8",
@@ -334,7 +335,7 @@ jQuery.ajax(baseUrl + '/save/contact', {
                   data: JSON.stringify(sendInfo) ,  // data to submit
                    dataType: 'json',
                   success: function (data, status, xhr) {
-                      var txt;
+                      var txt;                      
                       var r = confirm("Registration Successful, you will be redirected to login page now.");
                       if (r == true) {
                        // txt = "You pressed OK!";
@@ -344,23 +345,26 @@ jQuery.ajax(baseUrl + '/save/contact', {
                   },
                   error: function (jqXhr, textStatus, errorMessage) {
                     if(jqXhr.status == 200) {
-
+                      
                       var r = confirm("Registration Successful, you will be redirected to login page now.");
                       if (r == true) {
 
                         jQuery.when( jQuery.get("https://athenawpapi.azurewebsites.net/Register/GetUserId/"+email_id))
                           .then(function( data, textStatus, jqXHR ) {
-                          var userId = parseInt(jQuery.trim(data));
-                          if( userId > 0) {
+                          var userId = parseInt(jQuery.trim(data));                          
+                          if( userId > 0) {                            
                             //window.location.replace('https://ulearn.athena.edu/StudentEnroltoCourse?UId='+userId+'&CId='+cData.cId+'&ModId='+cData.modId);
-                            window.location.replace('https://athena.edu/StudentEnroltoCourse?UId='+userId+'&CId='+cid);
+                            window.location.replace('https://athena.edu/StudentEnroltoCourse?UId='+userId+'&CId='+cid+'&mail='+email_id);
 
                           }
+                          else{
+                            window.location.replace('https://athena.edu/StudentEnroltoCourse?mail='+email_id+'&CId='+cid);
+
+                          }
+
                         });
-
-
                         // window.location.replace('https://ulearn.athena.edu/login?mail='+email_id+'&CId='+cid);
-                        window.location.replace('https://athena.edu/StudentEnroltoCourse?mail='+email_id+'&CId='+cid);
+                        //window.location.replace('https://athena.edu/StudentEnroltoCourse?mail='+email_id+'&CId='+cid);
 
                       } else {
                         //txt = "You pressed Cancel!";
@@ -452,7 +456,7 @@ jQuery.ajax(baseUrl + '/save/contact', {
             var email_id = jQuery("#reg_email").val();
        var cid = jQuery("#reg_course").val();
                                 var redirect = confirm("Email ID already registered, redirect to login page ?");
-                                if (redirect == true) {
+                                if (redirect == true) {                                 
                                  // window.location.replace('https://ulearn.athena.edu/login?mail='+email_id+'&CId='+cid);
                                  window.location.replace('https://athena.edu/StudentEnroltoCourse?mail='+email_id+'&CId='+cid);
                                 } else {
