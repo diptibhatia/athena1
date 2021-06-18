a71c6143d8e5a901cf84b537e897f8fadd582de5
Dipti Bhatia <dipti@athena.edu>
2021-06-18 14:57:38 +0530

    url change

--- modules/custom/athena_course/src/Controller/ShortTermCourseController.php
+++ modules/custom/athena_course/src/Controller/ShortTermCourseController.php
@@ -27,11 +27,13 @@ class ShortTermCourseController {
             $this->_api = "/athenaprod";
         //$this->_lms_url = "https://newlms.athena.edu";
 
+        /*
         if (str_contains($_SERVER['HTTP_HOST'], 'websiteprod'))  
             $this->_curr_url = "http://learnstaging.athena.edu";
         else
             $this->_curr_url = $this->_lms_url; 
-        
+        */
+            $this->_curr_url = $this->_lms_url;
     }
 
     /**
