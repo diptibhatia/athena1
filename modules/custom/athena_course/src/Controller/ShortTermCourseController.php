23c796d9c866523e761015fe81057398421a6749
Dipti Bhatia <dipti@athena.edu>
2021-06-25 13:22:19 +0530

    Shortterm SMO Page Listing&Banner

--- modules/custom/athena_course/src/Controller/ShortTermCourseController.php
+++ modules/custom/athena_course/src/Controller/ShortTermCourseController.php
@@ -29,16 +29,21 @@ class ShortTermCourseController {
             $this->_api = "/athenaprod";
         //$this->_lms_url = "https://newlms.athena.edu";
 
+        if (in_array("subject", explode("/",$_SERVER['REQUEST_URI'] ))) $this->sublist = "SubjectList";
+        elseif (in_array("certificate", explode("/",$_SERVER['REQUEST_URI'] ))) $this->sublist = "CertList";
     }
 
     /**
      * List courses as per pagination.
      * @return [array]
      */
-    public function shortTermCourse() {
+    public function shortTermCourse( $id = NULL ) {
         $limit = $this->_limit;
         //$uri = "https://newlms.athena.edu/athenadev/api/courselist?page=1&page_limit=$limit";
         $uri = $this->_lms_url .$this->_api. "/api/courselist?page=1&page_limit=$limit&fk_type_of_qualification_id=1&status=1";
+
+        if ( $this->sublist == "SubjectList" ) $uri .= "&subject_area_id=".$id;
+        elseif ( $this->sublist == "CertList" ) $uri .= "&fk_certificate_type_id=".$id;
         try {
             $response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'application/json')));
             $data = (string)$response->getBody();
@@ -47,6 +52,8 @@ class ShortTermCourseController {
             $response_filters = \Drupal::httpClient()->get($filters_uri, array('headers' => array('Accept' => 'application/json')));
             $data_filters = (string)$response_filters->getBody();
             $filters = json_decode($data_filters, TRUE);
+
+            //print_r($filters);
 
             $certificates_uri = $this->_lms_url .$this->_api. "/api/get_master_table_data?table=certification_type";
             $response_certificates = \Drupal::httpClient()->get($certificates_uri, array('headers' => array('Accept' => 'application/json')));
@@ -118,8 +125,11 @@ class ShortTermCourseController {
             $display_load_more = FALSE;
         }
 
+        if (($this->sublist == "SubjectList" ) || ($this->sublist == "CertList" )) $theme =  "short_term_course_subject";
+        else $theme =  "short_term_course";
+
         $short_term_course =  [
-          '#theme' => 'short_term_course',
+          '#theme' => $theme,
           '#nodes' => $short_term_courses_nodes,
           '#filters' => $filters,
           '#certificates' => $certificates,
@@ -131,6 +141,16 @@ class ShortTermCourseController {
             ]
           ]
         ];
+
+        $short_term_course_subject =  [
+          '#SubOrCertList' => $this->sublist,
+          "#id" => $id,          
+      ];
+
+
+        if ( ($this->sublist == "SubjectList") || ($this->sublist == "CertList") ) 
+            $short_term_course = array_merge($short_term_course,$short_term_course_subject );
+
         return array(
            $short_term_course
         );
@@ -270,7 +290,7 @@ class ShortTermCourseController {
      * @param  [string] $query
      * @return [html]
      */
-    public function searchCourse($query) {
+    public function searchCourse( $query ) {
         $response = new AjaxResponse();
         $limit = $this->_limit;
         $query = trim($query);
@@ -284,7 +304,7 @@ class ShortTermCourseController {
 
         // $uri = "https://newlms.athena.edu/athenadev/api/courselist?page=1&page_limit=$limit&course_name=" . $query;
         $uri = $this->_lms_url .$this->_api. "/api/courselist?page=1&page_limit=$limit&fk_type_of_qualification_id=1&status=1" . $query;
-
+                
         $response_data = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'application/json')));
         $data = (string)$response_data->getBody();
 
@@ -388,6 +408,122 @@ class ShortTermCourseController {
                 </div>';
             }
             $response->addCommand(new HtmlCommand('.shortterm-courses', $html));
+        }
+
+        if ($total <= $limit) {
+            $response->addCommand(new InvokeCommand('.show_more_wrapper', 'hide'));
+        }
+        else {
+         $response->addCommand(new InvokeCommand('.show_more_wrapper', 'show'));
+        }
+
+        return $response;
+    }
+
+    /**
+     * Search more
+     * @param  [string] $query
+     * @return [html]
+     */
+    public function searchSubjectCourse( $id = NULL, $query ) {
+        $response = new AjaxResponse();
+        $limit = $this->_limit;
+        $query = trim($query);
+        $query = strip_tags($query);
+
+        if (empty($query)) {
+            $response->addCommand(new InvokeCommand('.show_more_wrapper', 'hide'));
+            $response->addCommand(new HtmlCommand('.shortterm-courses', 'No Content Found'));
+            return $response;
+        }
+
+        // $uri = "https://newlms.athena.edu/athenadev/api/courselist?page=1&page_limit=$limit&course_name=" . $query;
+        $uri = $this->_lms_url .$this->_api. "/api/courselist?page=1&page_limit=$limit&fk_type_of_qualification_id=1&status=1" . $query;
+                
+        $response_data = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'application/json')));
+        $data = (string)$response_data->getBody();
+
+        $nodes = json_decode($data);
+
+        $total = $nodes->data->total;
+        if ($total == 0) {
+            $response->addCommand(new InvokeCommand('.show_more_wrapper', 'hide'));
+            $response->addCommand(new HtmlCommand('.shortterm-courses', 'No Content Found'));
+            return $response;
+        }
+
+        $html = '';
+        if (count($nodes) > 0) {
+            foreach ($nodes->data->data as $key => $value) {
+                if (!empty($value->course_image_path)) {
+                    $course_image_path = $value->course_image_path;
+                }
+                else {
+                    $course_image_path = '/themes/custom/athena/images/course-image2.png';
+                }
+
+                foreach ($value->partner_body as $key1 => $value1) {
+                    if( is_object( $value1 )) {
+                        foreach($value1 as $key2 => $value2) {
+                           if ( $key2 == "logo" ) {
+                                $value2 = str_replace("pdfassets", "university", $value2);
+                                // $value2 = str_replace(".svg", "-white.svg", $value2);
+                                // $value2 = str_replace(".png", "-white.svg", $value2);
+                                $white_logo = $value2;
+                                if (!@getimagesize($white_logo)) {
+                                    $white_logo = athena_course_current_theme_image('images', 'no-university.png');
+                                }
+                           }
+                           if ( $key2 == "university_name" ) {
+                                $univ_name = $value2;
+                           }
+                        }
+                    }
+                }
+
+                $website_card_content = put_dots_in_string($value->website_card_content, 150);
+
+                $courses_data = [
+                    'cid' => $value->cid,
+                    'course_url' => $this->_lms_url . '/student-dashboard/course/' . $value->cid .'/'.$value->slug,
+                    'label' => $value->course_name,
+                    'body' => $website_card_content,
+                    'card_intro' => put_dots_in_string($value->course_introduction, 150),
+                    'field_course_amount' => 'Free',
+                    'white_logo' => $white_logo ?? athena_course_current_theme_image('images', 'no-university.png'),
+                    'univ_name' => $univ_name ?? '',
+                    'course_image' => $course_image_path
+                ];
+
+                $html .= '<div class="item content" style="display:block;">
+                            <div class="item-inner">                    
+                                <div class="course-item">
+                                    <div class="row heading m-0">
+                                        <div class="col-9">
+                                            <img src="' . $courses_data['white_logo'] . '" alt="' . $courses_data['univ_name'] . '">
+                                        </div>
+                                        <div class="col-3">
+                                            <span class="free-text">' . $courses_data['field_course_amount'] . '</span>
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="row">
+                                        <div class="image col-md-4">
+                                            <img src="' . $courses_data['course_image'] . '" alt="course-image">
+                                        </div>
+                                        <div class="course-details  col-md-8">
+                                            <h3>' . $courses_data['label'] . '</h3>
+                                            <div class="course-info">
+                                                ' . put_dots_in_string($courses_data['card_intro'], 150) . '
+                                            </div>
+                                            <div class="col-12 button-area"><a target="_blank" href="' . $courses_data['course_url'] . '"><button>Start Now</button></a></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>';
+            }
+            $response->addCommand(new HtmlCommand('.shortterm-smo-courses', $html));
         }
 
         if ($total <= $limit) {
